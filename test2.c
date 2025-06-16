5ec8aa7084e89d1e93aee50d6aa5b69fcf68147c
auttapol <027auttapol@gmail.com>
2025-06-16 09:59:26 +0700

    first commit

---
+++ test2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main() 
+{
+    int a,b,c=0;
+    printf("enter number a : ") ;
+    scanf("%d",&a);
+    printf("a= %d",a);
+     printf("\n enter number b : ") ;
+    scanf("%d",&b);
+    printf("b= %d",b);
+    c=a+b;
+    printf("c =a+b : %d",c );
+}
